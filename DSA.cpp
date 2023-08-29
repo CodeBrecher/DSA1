4ae1dc889dae0998cbf253a7c28d7dca059e1666
Dhruv Gupta <119490189+CodeBrecher@users.noreply.github.com>
2023-08-29 13:02:52 +0530

    Update DSA.cpp

--- DSA.cpp
+++ DSA.cpp
@@ -1,3 +1,33 @@
+// ARRAY APPROACH TO GET NUMBER OF TIMES THE CHARECTERS IS IN THE STRING
+char getMaxOccCharacter(string s) {
+
+    int arr[26] = {0};
+
+
+    //create an array of count of characters
+    for(int i=0; i<s.length(); i++) {
+        char ch = s[i];
+        //lowercase
+        int number = 0;
+        number = ch - 'a';
+        arr[number]++;
+    }
+
+    //find maximum occ character
+    int maxi = -1, ans = 0;
+    for(int i=0;i<26; i++) {
+        if(maxi < arr[i]) {
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+    
+
+    return 'a'+ans;
+
+}
+
+
 //SELECTION SORT
 
 #include <bits/stdc++.h> 
