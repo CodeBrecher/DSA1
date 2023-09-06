ba64d03e0bde5c8ddbfff082ce57e506de4c794b
Dhruv Gupta <119490189+CodeBrecher@users.noreply.github.com>
2023-09-06 11:56:46 +0530

    Update DSA.cpp

--- DSA.cpp
+++ DSA.cpp
@@ -154,3 +154,35 @@ int search(vector<int> &nums, int target)
         return count;
         
     }
+//FOE TAKING DYNAMIC INPUT IN CASE OF 2D ARRAYS
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int **arr = new int* [n];
+    for(int i =0; i<n; i++)
+    {
+        arr[i] = new int[n];
+    }
+    for(int i =0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    for(int i =0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
